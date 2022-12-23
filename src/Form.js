94fc4b623ad0cd63d8284397ba891b420290d734
Jonathan Eatroff <jonathan.eatroff@gmail.com>
2022-12-23 09:07:12 -0500

    Created form to add grocery

--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,7 @@ function Form() {
   return (
     <div className="App">
       <header className="App-header">
-  
+        <h1>Form</h1>
       </header>
     </div>
   );
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,17 @@
-
+import Form from "./Form";
 
 function NavBar() {
   return (
-    <div className="App">
-      <header className="App-header">
-  
-      </header>
-    </div>
+    <form className="new-grocery-form">
+      <input placeholder="Grocery" />
+      <select id="category">
+        <option value="" disabled selected hidden>Select a Category</option>
+        <option value="Fruits">Fruits</option>
+        <option value="Vegetables">Vegetables</option>
+        <option value="Meats">Meats</option>
+      </select>
+      <input type="submit" value="Add to List" />
+    </form>
   );
 }
 
